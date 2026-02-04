a71926ba4d4c748af21c9625bc2ba30197bbe9c5
UmeshBhabad <umeshbhabad9@gmail.com>
2026-02-04 20:51:08 +0530

    Added Classwork Day_11

---
+++ Classwork/Automation/Day_11/CommandLine1.py
@@ -0,0 +1,10 @@
+# main method
+def main():
+    No1 = int(input())          # accept input
+    No2 = int(input())
+
+    print(No1 + No2)            # display output
+
+# Start of program
+if __name__ == "__main__":
+    main()
